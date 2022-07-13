851a1263e5ddfdd31ccdd9982b662c646158e1d3
Joel Magnér <joel.magner@bonniernews.se>
2022-07-13 11:41:34 +0200

    esm-example

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const markoMiddleware = require("@marko/express").default;
+import express from "express";
+import fs from "fs";
+import markoMiddleware from "@marko/express";
+import vite from "vite";
+
 const isProd = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 3000;
 
+const dist = fs.readdirSync("./dist");
+
 (async () => {
-  const app = express()
-    .use(markoMiddleware());
+  const app = express().use(markoMiddleware());
 
   if (isProd) {
     app
       .use("/assets", express.static("dist/assets")) // Serve assets generated from vite.
-      .use(require("./dist").default);
+      .use(dist);
   } else {
-    const devServer = await require("vite").createServer({
-      server: { middlewareMode: true }
+    const devServer = await vite.createServer({
+      server: { middlewareMode: true },
     });
     app.use(devServer.middlewares);
     app.use(async (req, res, next) =>
-      (await devServer.ssrLoadModule("./src/index")).default(req, res, err => {
+      (await devServer.ssrLoadModule("./src/index")).default(req, res, (err) => {
         if (err) {
           devServer.ssrFixStacktrace(err);
           next(err);
@@ -28,7 +32,7 @@ const port = process.env.PORT || 3000;
     );
   }
 
-  app.listen(port, err => {
+  app.listen(port, (err) => {
     if (err) {
       throw err;
     }
